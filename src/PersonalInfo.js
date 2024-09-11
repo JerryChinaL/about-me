ed028e1b6605d5e493ddd002846a2b11ee130844
Jerry <66125613+JerryChinaL@users.noreply.github.com>
2024-09-10 21:16:46 -0500

    d

--- src/PersonalInfo.js
+++ src/PersonalInfo.js
@@ -5,7 +5,7 @@ function PersonalInfo() {
     return (
         <div className="PersonalInfo">
             <h1>About Me</h1>
-            <p>Hi, I'm Jerry. My best skills are mathematical thinking, programming, and machine learning. I have also developed an interest in the financial market and hope to learn more about trading and investing. Throughout my education, I wanted to make sure I had a broad skill set that would keep me adaptable and prepared for any opportunity down the road.</p>
+            <p>Hi, I'm Jerry. My best skills are mathematical thinking, programming, and machine learning. I also embraced opportunities related to trading and investing. During undergrad, I've acquired a broad skill set that would keep me adaptable and prepared for more opportunity in the future. </p>
             <p>In my free time, I enjoy watching movies, hiking, playing poker, and photography. I've been to 8 national parks in the US and I hope to explore more of them. My short plan is to take a good photo and hang it big on my wall. Further down the road, I want to drive on race tracks and get a pilot license.  </p>
         </div>
     );
